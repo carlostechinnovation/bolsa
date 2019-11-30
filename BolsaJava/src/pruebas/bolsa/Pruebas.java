8bbc522272ba97bbfac090b6b0421df0e12b9db8
Carlos <carlos.andres@correo.aeat.es>
2019-11-30 13:14:44 +0100

    Pruebas

---
+++ BolsaJava/src/pruebas/bolsa/Pruebas.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package pruebas.bolsa;
+
+/**
+ * @author casa
+ *
+ */
+public class Pruebas {
+
+	/**
+	 * 
+	 */
+	public Pruebas() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Bolsa - Pruebas - INICIO");
+
+		System.out.println("Bolsa - Pruebas - FIN");
+	}
+
+}
