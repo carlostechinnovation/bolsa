aa1ec9699967ab3a24835aa8e8f6914ee34e7173
Carlos <carlos.andres@correo.aeat.es>
2019-11-30 15:46:25 +0100

    Primeros pasos en KNIME y Java

--- BolsaJava/src/pruebas/bolsa/Pruebas.java
+++ BolsaJava/src/pruebas/bolsa/Pruebas.java
@@ -20,8 +20,6 @@ public class Pruebas {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		System.out.println("Bolsa - Pruebas - INICIO");
 
 		System.out.println("Bolsa - Pruebas - FIN");
