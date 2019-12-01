d5417286d3bc3ff14af3984aa668eb096d88fa5a
Carlos <carlos.andres@correo.aeat.es>
2019-12-01 14:28:00 +0100

    Reorganizados los paquetes Java. Nuevo proyecto de scripts

---
+++ BolsaScripts/master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Script principal COORDINADOR de todas las tareas. Lo que no quiera ejecutar, lo comento.
+DIR_BASE="/home/carloslinux/Desktop/BOLSA/"
+PATH_LOG="${DIR_BASE}coordinador.log"
+PATH_SCRIPTS="/home/carloslinux/git/bolsa/BolsaScripts/"
+
+
+
+
+
+
+
+
