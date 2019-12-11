c82464a75e2d11dd36a0026afec4a0c388c7f91b
Carlos <carlos.andres@correo.aeat.es>
2019-12-11 14:28:22 +0100

    Capa 5 (PCA) y otras cosas ligeras

--- BolsaPython/ejemplos/01_introduccion.py
+++ BolsaPython/ejemplos/01_introduccion.py
@@ -1,5 +1,8 @@
 from sklearn import datasets
 
+import pandas as pd
+from sklearn.decomposition import PCA
+
 print("----------- DATOS (pasado + futuro) ----------")
 print("Datasets de ejemplo")
 iris = datasets.load_iris()
@@ -29,4 +32,18 @@ print("Predicción del target para la última fila (para el ejemplo)...")
 features_futuro=digits.data[-1:]
 targets_futuro=modeloLeido.predict(features_futuro)
 print(*targets_futuro, sep = ", ")
+
+
+
+print("Cargar datos (CSV)...")
+entradaFeaturesYTarget = pd.read_csv(filepath_or_buffer="C:\\bolsa\\pasado\\datasets\\1.csv", sep='|')
+entradaFeaturesYTarget.head()
+features = entradaFeaturesYTarget.drop('TARGET', axis=1)
+targets = entradaFeaturesYTarget[['TARGET']]
+
+print("Usando PCA, cogemos las features que tengan un impacto agregado sobre el X% de la varianza del target. Descartamos el resto.")
+my_model = PCA(n_components=0.95, svd_solver='full')
+features_reducidas = my_model.fit_transform(features)
+
+x=0
 
