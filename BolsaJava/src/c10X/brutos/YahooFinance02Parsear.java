44f2c508e9fe35fee876e223a7e4398a77beae93
Carlos Andres <carlosandresgarcia1986@hotmail.com>
2019-12-23 17:21:35 +0100

    Config

--- BolsaJava/src/c10X/brutos/YahooFinance02Parsear.java
+++ BolsaJava/src/c10X/brutos/YahooFinance02Parsear.java
@@ -8,7 +8,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
