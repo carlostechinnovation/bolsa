618c62c15c2c83faac28ffd5077310ad67b92948
Carlos Andres <carlosandresgarcia1986@hotmail.com>
2019-12-28 23:43:27 +0100

    capas

--- BolsaPython/ejemplos/pruebas.py
+++ BolsaPython/ejemplos/pruebas.py
@@ -1,5 +1,11 @@
 print("--- PRUEBAS ---")
-entrada_csv_subgrupo = "/bolsa/pasado/subgrupos/SG_0/"
-modoTiempo = "pasado"
+entrada_csv_subgrupo = "/bolsa/futuro/subgrupos/SG_0/"
+modoTiempo = "futuro"
+
+if modoTiempo == "futuro":
+    entrada_csv_subgrupo = entrada_csv_subgrupo.replace("futuro", "pasado")
+    print(entrada_csv_subgrupo)
+
+
 
 
