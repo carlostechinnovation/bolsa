ab76807f1ebe7b36591f5916df228745134ec824
landgar <luisanga1986@msn.com>
2020-01-21 22:31:57 +0100

    cambio en comentario

--- BolsaScripts/validacionEuros.sh
+++ BolsaScripts/validacionEuros.sh
@@ -16,7 +16,7 @@ X="4"  #Duracion en velas de [t1,t2]
 R="10"  #Caida ligera máxima permitida durante [t2,t3], en TODAS esas velas.
 M="1"  #Duración en velas de [t2,t3]
 F="5"  #Caida ligera permitida durante [t2,t3], en la ÚLTIMA vela
-B="5"  #Caida ligera permitida durante [t1,t2], en todas esas velas
+B="5"  #Usar B = F. Caida ligera permitida durante [t1,t2], en todas esas velas
 NUM_EMPRESAS="1000"  #Numero de empresas descargadas
 ACTIVAR_DESCARGAS="N" #Descargar datos nuevos (S) o usar datos locales (N)
 UMBRAL_SUBIDA_POR_VELA="0.3" #Usar = (1/X + 0.05). Umbral de subida máxima relativa de una vela respecto de la subida de 1 a X velas. Se recomienda poner un valor ligeramente inferior a "1/X".  Por ejemplo, si X=4, tomar 0.3. NO poner justamente 1/X, porque da peores resultados 
