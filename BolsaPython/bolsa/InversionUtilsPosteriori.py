99c0d73039c4772399f25ca2ad672af31922d266
landgar <luisanga1986@msn.com>
2020-04-02 17:06:29 +0200

    análisis: se loggea por día el rendimiento medio

--- BolsaPython/bolsa/InversionUtilsPosteriori.py
+++ BolsaPython/bolsa/InversionUtilsPosteriori.py
@@ -1,15 +1,94 @@
 import sys
 import os
 import pandas as pd
+import glob
+import numpy as np
+
+#EXPLICACIÓN:
+#1º. inversion.sh ha generado unos 100 ficheros al dia: 50 son de 300KB (tienen un pequeño histórico) y otros son de 5KB (antiguedad=0)
+#2º. Entonces, en PYTHON, vamos a recorrer todos los CSV de 5KB y vamos a construir una tabla (DATAFRAME) que tenga -->  DIA_INVERSION, DIA_POSTERIORI, subgrupo, empresa, target_predicho, target_predicho_PROB
+#3º. En PYTHON, vamos a recorrer todos los CSVs de 300 KB y vamos a construir una tabla (DATAFRAME) que tenga --> DIA, EMPRESA, precio_close, target (es el target REAL)
+#4º. Comparamos las dos tablas --> En la tabla 1 metemos la columna de qué ha pasado realmente en el futuro (precio, target REAL)
+#5º. Graficas del rendimiento...
 
 print("--- InversionUtilsPosteriori: INICIO ---")
 
+dirDropbox = sys.argv[1]
+dirAnalisis = sys.argv[2]
+S = sys.argv[3]
+X = sys.argv[4]
+R = sys.argv[5]
+M = sys.argv[6]
+F = sys.argv[7]
+B = sys.argv[8]
 
+# Se listan los ficheros manejables y grandes (sólo los má recientes, con antigüedad 0 en el nombre)
+ficherosManejables = glob.glob(dirDropbox+"*MANEJABLE*")
+ficherosGrandesCero = glob.glob(dirDropbox+"*GRANDE_0*")
 
+#Se iteran los ficheros manejables, para leer su contenido
+columnasManejables=['empresa', 'antiguedad', 'anio', 'mes', 'dia', 'hora', 'close', 'TARGET_PREDICHO', 'TARGET_PREDICHO_PROB']
+datosManejables=pd.DataFrame()
+contenidosManejables=[]
 
+for filename in ficherosManejables:
+    datosFichero = pd.read_csv(filepath_or_buffer=filename, sep='|')
+    datosFicheroReducidos=datosFichero[columnasManejables]
+    datosFicheroInteresantes=pd.DataFrame(datosFicheroReducidos.loc[datosFicheroReducidos['TARGET_PREDICHO'] == 1],
+                                          columns=columnasManejables)
+    contenidosManejables.append(datosFicheroInteresantes)
 
+datosManejables=datosManejables.append(contenidosManejables)
 
-print("--- InversionUtilsPosteriori: FIN ---")
+#Se iteran los ficheros grandes, para extraer su contenido
+columnasGrandes=['empresa', 'antiguedad', 'anio', 'mes', 'dia', 'hora', 'close', 'TARGET']
+datosGrandes=pd.DataFrame()
+contenidosGrandes=[]
+
+for filename in ficherosGrandesCero:
+    datosFichero = pd.read_csv(filepath_or_buffer=filename, sep='|')
+    datosFicheroReducidos=datosFichero[columnasGrandes]
+    datosFicheroInteresantes=pd.DataFrame(datosFicheroReducidos, columns=columnasGrandes)
+    contenidosGrandes.append(datosFicheroInteresantes)
+
+datosGrandes=datosGrandes.append(contenidosGrandes)
+
+#Se mergean los datos, separados por dia y subgrupo
+#Se ubican los manejables dentro de los grandes. Así puedo conocer su antigüedad
+datosMergeados = pd.merge(datosGrandes, datosManejables, how='right', on=['empresa', 'anio', 'mes', 'dia', 'hora', 'close'])
+
+#De entre los grandes, se escogen aquéllos cuya antigüedad sea la de los "manejables - X" (vamos hacia el futuro)
+datosPredichos=pd.merge(datosGrandes, datosManejables, how='left', on=['empresa'])
+#Desplazo la antiguedad de los mergeados: resto X velas
+datosMergeados['antiguedad_x'] -= int(X)
+# Relleno, de entre los grandes predichos, los de empresa y antiguedad desplazada X
+datosSeleccionados=pd.merge(datosPredichos, datosMergeados, how='left', on=['empresa', 'antiguedad_x'])
+
+#Se filtran los que tengan target real (calculado con la realidad futura) rellena
+datosAAnalizar=datosSeleccionados.loc[datosSeleccionados['TARGET_y'] == 1]
+
+#Se clasifican/agrupan los datos por mes+día (PENDIENTE HACERLO TAMBIÉN POR SUBGRUPO)
+#En fecha_x está el futuro. En fecha_y está el dato predicho. Se genera una columna nueva que obtiene el rendimiento real (close_x vs close_y)
+datosAAnalizar['rendimiento'] = 100 * (datosAAnalizar['close_x']-datosAAnalizar['close_y'])/datosAAnalizar['close_y']
+
+grupos=datosAAnalizar.groupby('antiguedad_x')
+#Se calculan probabilidades, y se loggean/grafican por antiguedad
+for group_name, df_group in grupos:
+    #En cada antigüedad se reinician los contadores
+    rentaMedia=0
+    rentasAcumuladas=[]
+
+    for row_index, row in df_group.iterrows():
+        rentasAcumuladas.append(row['rendimiento'])
+
+    # Se calculan las rentas
+    rentaMedia = np.mean(rentasAcumuladas)
+
+    #Se imprimen los resultados
+    print('\nANTIGUEDAD {}: '.format(group_name))
+    print(' RENTABILIDAD MEDIA {}'.format(rentaMedia))
+
+print("\n--- InversionUtilsPosteriori: FIN ---")
 
 
 
--- BolsaScripts/inversion_posteriori.sh
+++ BolsaScripts/inversion_posteriori.sh
@@ -2,18 +2,84 @@
 
 echo -e "INVERSION POSTERIORI - INICIO: "$( date "+%Y%m%d%H%M%S" )
 
+#################### DIRECTORIOS ###############################################################
+DIR_DROPBOX_CARLOS="/home/carloslinux/Dropbox/BOLSA_PREDICTOR/"
+DIR_DROPBOX_LUIS="/home/t151521/Dropbox/BOLSA_PREDICTOR/"
+PYTHON_MOTOR_CARLOS="/home/carloslinux/Desktop/PROGRAMAS/anaconda3/envs/BolsaPython/bin/python"
+PYTHON_MOTOR_LUIS="/home/t151521/anaconda3/envs/BolsaPython/bin/python"
 
+usuario=$(whoami)
+if [ $usuario == "carloslinux" ]
+then
+  DIR_DROPBOX="${DIR_DROPBOX_CARLOS}"
+  PYTHON_MOTOR="${PYTHON_MOTOR_CARLOS}"
+elif [ $usuario == "t151521" ]
+then
+  DIR_DROPBOX="${DIR_DROPBOX_LUIS}"
+  PYTHON_MOTOR="${PYTHON_MOTOR_LUIS}"
+else
+  echo "ERROR: USUARIO NO CONTROLADO"
+  s1 -e
+fi
 
-1º. inversion.sh ha generado unos 100 ficheros al dia: 50 son de 300KB (tienen un pequeño historico) y otros son de 5KB (antiguedad=0)
+################## PARAMETROS DE ENTRADA ############################################
+# PARAMETROS DE TARGET MEDIDOS EN VELAS
+S="${1}" #default=10
+X="${2}" #default=56
+R="${3}" #default=10
+M="${4}" #default=7
+F="${5}" #default=5
+B="${6}" #default=5
 
-2º. Entonces, en PYTHON, vamos a recorrer todos los CSV de 5KB y vamos a construir una tabla (DATAFRAME) que tenga -->  DIA_INVERSION, DIA_POSTERIORI, subgrupo, empresa, target_predicho, target_predicho_PROB
+################## FUNCIONES #############################################################
+crearCarpetaSiNoExiste() {
+	param1=${1} 			#directorio
+	echo "Creando carpeta: $param1"
+	mkdir -p ${param1}
+}
 
-3º. En PYTHON, vamos a recorrer todos los CSVs de 300 KB y vamos a construir una tabla (DATAFRAME) que tenga --> DIA, EMPRESA, precio_close, target (es el target REAL)
+crearCarpetaSiNoExisteYVaciar() {
+	param1=${1} 			#directorio
+	echo "Creando carpeta: $param1"
+	mkdir -p ${param1}
+	rm -f ${param1}*
+}
 
-4º. Comparamos las dos tablas --> En la tabla 1 metemos la columna de qué ha pasado realmente en el futuro (precio, target REAL) 
+crearCarpetaSiNoExisteYVaciarRecursivo() {
+	param1=${1} 			#directorio
+	echo "Creando carpeta: $param1"
+	mkdir -p ${param1}
+	rm -Rf ${param1}*
+}
 
-5º. Graficas del rendimiento...
+################ VARIABLES DE EJECUCION #########################################################
+ID_EJECUCION=$( date "+%Y%m%d%H%M%S" )
+echo -e "ID_EJECUCION = "${ID_EJECUCION}
 
+PATH_SCRIPTS="${DIR_CODIGOS}BolsaScripts/"
+PYTHON_SCRIPTS="${DIR_CODIGOS}BolsaPython/"
+DIR_JAVA="${DIR_CODIGOS}BolsaJava/"
+PATH_JAR="${DIR_JAVA}target/bolsajava-1.0-jar-with-dependencies.jar"
+
+DIR_BASE="/bolsa/"
+DIR_ANALISIS="${DIR_DROPBOX}ANALISIS/"
+DIR_BRUTOS_CSV="${DIR_BASE}futuro/brutos_csv/"
+
+crearCarpetaSiNoExiste "${DIR_ANALISIS}"
+
+############### LOGS ########################################################
+rm -f "${DIR_LOGS}log4j.log"
+LOG_MASTER="${DIR_LOGS}${ID_EJECUCION}_bolsa_coordinador_analisis.log"
+rm -f "${LOG_MASTER}"
+
+############### COMPILAR JAR ########################################################
+echo -e "Compilando JAVA en un JAR..." >> ${LOG_MASTER}
+cd "${DIR_JAVA}" >> ${LOG_MASTER}
+rm -Rf "${DIR_JAVA}target/" >> ${LOG_MASTER}
+mvn clean compile assembly:single >> ${LOG_MASTER}
+
+################################################################################################
+$PYTHON_MOTOR "${PYTHON_SCRIPTS}bolsa/InversionUtilsPosteriori.py" "${DIR_DROPBOX}" "${DIR_ANALISIS}" "${S}" "${X}" "${R}" "${M}" "${F}" "${B}" >> ${LOG_MASTER}
 
 echo -e "INVERSION POSTERIORI - FIN: "$( date "+%Y%m%d%H%M%S" )
 
