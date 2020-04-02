8ade37929772796917a36b478471b3b2990dc11c
landgar <luisanga1986@msn.com>
2020-04-02 19:12:33 +0200

    correcciones análisis inversión

--- BolsaScripts/inversion_posteriori.sh
+++ BolsaScripts/inversion_posteriori.sh
@@ -3,33 +3,28 @@
 echo -e "INVERSION POSTERIORI - INICIO: "$( date "+%Y%m%d%H%M%S" )
 
 #################### DIRECTORIOS ###############################################################
-DIR_DROPBOX_CARLOS="/home/carloslinux/Dropbox/BOLSA_PREDICTOR/"
-DIR_DROPBOX_LUIS="/home/t151521/Dropbox/BOLSA_PREDICTOR/"
 PYTHON_MOTOR_CARLOS="/home/carloslinux/Desktop/PROGRAMAS/anaconda3/envs/BolsaPython/bin/python"
 PYTHON_MOTOR_LUIS="/home/t151521/anaconda3/envs/BolsaPython/bin/python"
+DIR_CODIGOS_CARLOS="/home/carloslinux/Desktop/GIT_BOLSA/"
+DIR_CODIGOS_LUIS="/home/t151521/bolsa/"
+DIR_DROPBOX_CARLOS="/home/carloslinux/Dropbox/BOLSA_PREDICTOR/"
+DIR_DROPBOX_LUIS="/home/t151521/Dropbox/BOLSA_PREDICTOR/"
 
 usuario=$(whoami)
 if [ $usuario == "carloslinux" ]
 then
-  DIR_DROPBOX="${DIR_DROPBOX_CARLOS}"
+  DIR_CODIGOS="${DIR_CODIGOS_CARLOS}"
   PYTHON_MOTOR="${PYTHON_MOTOR_CARLOS}"
+  DIR_DROPBOX="${DIR_DROPBOX_CARLOS}"
 elif [ $usuario == "t151521" ]
 then
-  DIR_DROPBOX="${DIR_DROPBOX_LUIS}"
+  DIR_CODIGOS="${DIR_CODIGOS_LUIS}"
   PYTHON_MOTOR="${PYTHON_MOTOR_LUIS}"
+  DIR_DROPBOX="${DIR_DROPBOX_LUIS}"
 else
   echo "ERROR: USUARIO NO CONTROLADO"
   s1 -e
 fi
-
-################## PARAMETROS DE ENTRADA ############################################
-# PARAMETROS DE TARGET MEDIDOS EN VELAS
-S="${1}" #default=10
-X="${2}" #default=56
-R="${3}" #default=10
-M="${4}" #default=7
-F="${5}" #default=5
-B="${6}" #default=5
 
 ################## FUNCIONES #############################################################
 crearCarpetaSiNoExiste() {
@@ -57,11 +52,17 @@ ID_EJECUCION=$( date "+%Y%m%d%H%M%S" )
 echo -e "ID_EJECUCION = "${ID_EJECUCION}
 
 PATH_SCRIPTS="${DIR_CODIGOS}BolsaScripts/"
+PATH_SCRIPTS="${DIR_CODIGOS}BolsaScripts/"
 PYTHON_SCRIPTS="${DIR_CODIGOS}BolsaPython/"
+PARAMS_CONFIG="${PATH_SCRIPTS}parametros.config"
+echo -e "Importando parametros generales..."
+source ${PARAMS_CONFIG}
+
 DIR_JAVA="${DIR_CODIGOS}BolsaJava/"
 PATH_JAR="${DIR_JAVA}target/bolsajava-1.0-jar-with-dependencies.jar"
 
 DIR_BASE="/bolsa/"
+DIR_LOGS="${DIR_BASE}logs/"
 DIR_ANALISIS="${DIR_DROPBOX}ANALISIS/"
 DIR_BRUTOS_CSV="${DIR_BASE}futuro/brutos_csv/"
 
@@ -71,12 +72,6 @@ crearCarpetaSiNoExiste "${DIR_ANALISIS}"
 rm -f "${DIR_LOGS}log4j.log"
 LOG_MASTER="${DIR_LOGS}${ID_EJECUCION}_bolsa_coordinador_analisis.log"
 rm -f "${LOG_MASTER}"
-
-############### COMPILAR JAR ########################################################
-echo -e "Compilando JAVA en un JAR..." >> ${LOG_MASTER}
-cd "${DIR_JAVA}" >> ${LOG_MASTER}
-rm -Rf "${DIR_JAVA}target/" >> ${LOG_MASTER}
-mvn clean compile assembly:single >> ${LOG_MASTER}
 
 ################################################################################################
 $PYTHON_MOTOR "${PYTHON_SCRIPTS}bolsa/InversionUtilsPosteriori.py" "${DIR_DROPBOX}" "${DIR_ANALISIS}" "${S}" "${X}" "${R}" "${M}" "${F}" "${B}" >> ${LOG_MASTER}
