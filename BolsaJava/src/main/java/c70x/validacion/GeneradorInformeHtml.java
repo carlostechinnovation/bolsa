75904bc7b6b1a1baf76d2c088b223487bc7a9b84
Carlos Andres <carlosandresgarcia1986@hotmail.com>
2020-11-29 12:02:22 +0100

    Test de integracion para analizar una empresa de un subgrupo

--- BolsaJava/src/main/java/c70x/validacion/GeneradorInformeHtml.java
+++ BolsaJava/src/main/java/c70x/validacion/GeneradorInformeHtml.java
@@ -147,7 +147,7 @@ public class GeneradorInformeHtml implements Serializable {
 		outSB.append("Ficheros de FINVIZ (datos estaticos): " + brutosFinviz.size() + "<br>");
 //		brutosFinviz.forEach(outSB::append);
 
-		List<String> brutosFinvizInsiders = Files.walk(Paths.get(dirTiempo + "brutos_csv")).map(x -> x.toString())
+		List<String> brutosFinvizInsiders = Files.walk(Paths.get(dirTiempo + "brutos_csv_OLD")).map(x -> x.toString())
 				.filter(f -> f.contains("/" + BrutosUtils.FINVIZ_INSIDERS) && f.endsWith(".csv"))
 				.collect(Collectors.toList());
 		outSB.append("Ficheros de FINVIZ (datos dinamicos de operaciones de insiders): " + brutosFinvizInsiders.size()
--- BolsaScripts/testIntegracion.sh
+++ BolsaScripts/testIntegracion.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #set -e
 echo -e "TEST INTEGRACION - INICIO: "$( date "+%Y%m%d%H%M%S" )
-LOG_INTEGRACION="/bolsa/logs/integracion.log"
-echo -e ""  > ${LOG_INTEGRACION}
+INFORME_OUT="/bolsa/logs/integracion.html"
+echo -e "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"></head><body>"  > ${INFORME_OUT}
 
 #################### DIRECTORIOS ###############################################################
 DIR_CODIGOS_CARLOS="/home/carloslinux/Desktop/GIT_BOLSA/"
@@ -48,98 +48,100 @@ crearCarpetaSiNoExisteYVaciarRecursivo() {
 ############### COMPILAR JAR ########################################################
 DIR_JAVA="${DIR_CODIGOS}BolsaJava/"
 PATH_JAR="${DIR_JAVA}target/bolsajava-1.0-jar-with-dependencies.jar"
-echo -e "Compilando JAVA en un JAR..." >> ${LOG_INTEGRACION}
-cd "${DIR_JAVA}" >> ${LOG_INTEGRACION}
-rm -Rf "${DIR_JAVA}target/" >> ${LOG_INTEGRACION}
-mvn clean compile assembly:single  # >> ${LOG_INTEGRACION}
+echo -e "Compilando JAVA en un JAR...<br>" >> ${INFORME_OUT}
+cd "${DIR_JAVA}" >> ${INFORME_OUT}
+rm -Rf "${DIR_JAVA}target/" >> ${INFORME_OUT}
+mvn clean compile assembly:single 1>/dev/null  2>> ${INFORME_OUT}
 
 #######################################################################################################
-echo -e "Damos por hecho que tenemos una ejecucion completa de PASADO+FUT1-FUT2"  >> ${LOG_INTEGRACION}
-echo -e "Cogemos una empresa y vemos su evolucion en cada capa."  >> ${LOG_INTEGRACION}
+echo -e "Damos por hecho que tenemos una ejecucion completa de PASADO+FUT1-FUT2<br>"  >> ${INFORME_OUT}
+echo -e "Cogemos una empresa y vemos su evolucion en cada capa.<br>"  >> ${INFORME_OUT}
 
 #######################################################################################################
-echo -e "******************************** COMPROBACIONES del PASADO *******************************" >> ${LOG_INTEGRACION}
-echo -e "Elegimos un subgrupo y empresa al azar para el que tengamos datos hasta la última capa..." >> ${LOG_INTEGRACION}
+echo -e "<h2>******************************** COMPROBACIONES del PASADO *******************************</h2>" >> ${INFORME_OUT}
+echo -e "Elegimos un subgrupo y empresa al azar para el que tengamos datos hasta la última capa...<br>" >> ${INFORME_OUT}
 SG_ANALIZADO=$(find "/bolsa/pasado/subgrupos/" | grep "REDUCIDO" | shuf -n 1 | cut -d'/' -f5)
-echo -e "Subgrupo analizado: ${SG_ANALIZADO}" >> ${LOG_INTEGRACION}
+echo -e "<b>Subgrupo analizado: ${SG_ANALIZADO}</b>" >> ${INFORME_OUT}
 empresa=$(cat /bolsa/pasado/subgrupos/${SG_ANALIZADO}/EMPRESAS.txt | head -n 1 | tr -d '\n' | cut -d'/' -f5 | cut -d'.' -f1 | cut -d'_' -f2)
-echo -e "Empresa analizada: ${empresa}" >> ${LOG_INTEGRACION}
+echo -e "<br><b>Empresa analizada: ${empresa}</b>" >> ${INFORME_OUT}
 #####
-echo -e "\n-- Capa 1.1 (brutos desestructurados) ---" >> ${LOG_INTEGRACION}
+echo -e "<br><h3>Capa 1.1 (brutos desestructurados)</h3>" >> ${INFORME_OUT}
 BRUTO_YF="/bolsa/pasado/brutos/YF_NASDAQ_${empresa}.txt"
 BRUTO_FZ="/bolsa/pasado/brutos/FZ_NASDAQ_${empresa}.html"
-echo -e "Bruto - YahooFinance: ${BRUTO_YF} --> Tamanio (bytes) = "$(stat -c%s "$BRUTO_YF") >> ${LOG_INTEGRACION}
-echo -e "Bruto - Finviz - Datos estáticos: ${BRUTO_FZ} --> Tamanio (bytes) = "$(stat -c%s "$BRUTO_FZ") >> ${LOG_INTEGRACION}
+echo -e "Bruto - YahooFinance: ${BRUTO_YF} --> Tamanio (bytes) = "$(stat -c%s "$BRUTO_YF") >> ${INFORME_OUT}
+echo -e "<br>Bruto - Finviz - Datos estáticos: ${BRUTO_FZ} --> Tamanio (bytes) = "$(stat -c%s "$BRUTO_FZ") >> ${INFORME_OUT}
 #####
-echo -e "\n-- Capa 1.2 (brutos estructurados) ---" >> ${LOG_INTEGRACION}
+echo -e "<br><h3>Capa 1.2 (brutos estructurados)</h3>" >> ${INFORME_OUT}
 BRUTO_CSV="/bolsa/pasado/brutos_csv/NASDAQ_${empresa}.csv"
-echo -e "Bruto CSV: ${BRUTO_CSV} --> Tamanio (bytes) = "$(stat -c%s "$BRUTO_CSV")" con "$(wc -l $BRUTO_CSV | cut -d\  -f 1)" filas" >> ${LOG_INTEGRACION}
-head -n 10 ${BRUTO_CSV}>> ${LOG_INTEGRACION}
+echo -e "Bruto CSV: ${BRUTO_CSV} --> Tamanio (bytes) = "$(stat -c%s "$BRUTO_CSV")" con "$(wc -l $BRUTO_CSV | cut -d\  -f 1)" filas<br>" >> ${INFORME_OUT}
+head -n 10 ${BRUTO_CSV}>> ${INFORME_OUT}
 #####
-echo -e "\n-- Capa 2 (limpios) ---" >> ${LOG_INTEGRACION}
+echo -e "<br><h3>Capa 2 (limpios)</h3>" >> ${INFORME_OUT}
 LIMPIO="/bolsa/pasado/limpios/NASDAQ_${empresa}.csv"
-echo -e "Limpio: ${LIMPIO} --> Tamanio (bytes) = "$(stat -c%s "$LIMPIO")" con "$(wc -l $LIMPIO | cut -d\  -f 1)" filas\n" >> ${LOG_INTEGRACION}
-head -n 5 ${LIMPIO}>> ${LOG_INTEGRACION}
+echo -e "Limpio: ${LIMPIO} --> Tamanio (bytes) = "$(stat -c%s "$LIMPIO")" con "$(wc -l $LIMPIO | cut -d\  -f 1)" filas<br>" >> ${INFORME_OUT}
+head -n 5 ${LIMPIO}>> ${INFORME_OUT}
 #####
-echo -e "\n-- Capa 3 (elaboradas) ---" >> ${LOG_INTEGRACION}
+echo -e "<br><h3>Capa 3 (elaboradas)</h3>" >> ${INFORME_OUT}
 ELABORADO="/bolsa/pasado/elaborados/NASDAQ_${empresa}.csv"
-echo -e "Elaborado: ${ELABORADO} --> Tamanio (bytes) = "$(stat -c%s "$ELABORADO")" con "$(wc -l $ELABORADO | cut -d\  -f 1)" filas\n" >> ${LOG_INTEGRACION}
-head -n 5 ${ELABORADO}>> ${LOG_INTEGRACION}
+echo -e "Elaborado: ${ELABORADO} --> Tamanio (bytes) = "$(stat -c%s "$ELABORADO")" con "$(wc -l $ELABORADO | cut -d\  -f 1)" filas<br>" >> ${INFORME_OUT}
+head -n 5 ${ELABORADO}>> ${INFORME_OUT}
 #####
-echo -e "\n-- Capa 4 (subgrupos) ---" >> ${LOG_INTEGRACION}
+echo -e "<br><h3>Capa 4 (subgrupos)</h3>" >> ${INFORME_OUT}
 DIR_SUBGRUPOS="/bolsa/pasado/subgrupos/"
-echo -e "Subgrupos creados (los que superan suficientes requisitos) -> "$(ls $DIR_SUBGRUPOS)"\n" >> ${LOG_INTEGRACION}
+echo -e "Subgrupos creados (los que superan suficientes requisitos) -> "$(ls $DIR_SUBGRUPOS)"<br>" >> ${INFORME_OUT}
 SG_EMPRESAS="${DIR_SUBGRUPOS}${SG_ANALIZADO}/EMPRESAS.txt"
-echo -e "Subgrupo ${SG_ANALIZADO} - La empresa aparece en la lista de empresas del subgrupo: --> "$(cat "$SG_EMPRESAS" | grep ${empresa}) >> ${LOG_INTEGRACION}
+echo -e "<br>El subgrupo analizado (elegido al azar) es: ${SG_ANALIZADO} --> La empresa analizada aparece en la lista de empresas de ese subgrupo: --> "$(cat "$SG_EMPRESAS" | grep '_${empresa}\.') >> ${INFORME_OUT}
 #####
-echo -e "\n-- Capa 5 (reducir CSV y entrenar modelo) ---" >> ${LOG_INTEGRACION}
-echo -e "\n\nFlujo de datos en capa 5:     COMPLETO.csv --> Transformaciones......  --> REDUCIDO.csv\n" >> ${LOG_INTEGRACION}
+echo -e "<br><h3>Capa 5 (reducir CSV y entrenar modelo)</h3><br>" >> ${INFORME_OUT}
+echo -e "Flujo de datos en capa 5:     COMPLETO.csv --> Transformaciones......  --> REDUCIDO.csv<br>" >> ${INFORME_OUT}
 SG_ENTRADA="${DIR_SUBGRUPOS}${SG_ANALIZADO}/COMPLETO.csv"
-echo -e "Subgrupo ${SG_ANALIZADO} - Datos entrada: ${SG_ENTRADA} --> Tamanio (bytes) = "$(stat -c%s "$SG_ENTRADA")" con "$(wc -l $SG_ENTRADA | cut -d\  -f 1)" filas\n" >> ${LOG_INTEGRACION}
-echo -e "Ejemplo de filas de la empresa dentro de COMPLETO.csv:" >> ${LOG_INTEGRACION}
-head -n 1 ${SG_ENTRADA}   >> ${LOG_INTEGRACION}  # Cabecera
-cat ${SG_ENTRADA} | grep "${empresa}" | head -n 4 >> ${LOG_INTEGRACION}
+echo -e "Subgrupo ${SG_ANALIZADO} - Datos entrada: ${SG_ENTRADA} --> Tamanio (bytes) = "$(stat -c%s "$SG_ENTRADA")" con "$(wc -l $SG_ENTRADA | cut -d\  -f 1)" filas<br>" >> ${INFORME_OUT}
+echo -e "<br>Ejemplo de filas de la empresa dentro de COMPLETO.csv:" >> ${INFORME_OUT}
+head -n 1 ${SG_ENTRADA}   >> ${INFORME_OUT}  # Cabecera
+cat ${SG_ENTRADA} | grep "${empresa}" | head -n 4 >> ${INFORME_OUT}
 #####
-echo -e "Para analizar el CSV reducido (normalizado, sin nulos y solo con features dinámicas elegidas), veamos las primeras filas del fichero de entrada (COMPLETO.csv) y las primeras filas del fichero reducido (serán esas mismas filas, salvo si tenían nulos, que habrán sido eliminadas y veríamos las siguientes)" >> ${LOG_INTEGRACION}
-echo -e "\nLas primeras filas de COMPLETO (de la primera empresa que aparece):\n" >> ${LOG_INTEGRACION}
-head -n 4 "${SG_ENTRADA}" >> ${LOG_INTEGRACION}
-echo -e "\nLas features elegidas han sido:\n" >> ${LOG_INTEGRACION}
-FEATURES_ELEGIDAS=$(head -n 1 ${SG_REDUCIDO})
-echo -e "${FEATURES_ELEGIDAS}" >> ${LOG_INTEGRACION}
-
-echo -e "\n\nEntonces, nos fijamos solo en esas features elegidas de COMPLETO:\n" >> ${LOG_INTEGRACION}
-TMP_ENTRADA_COLUMNAS_ELEGIDAS="/tmp/temp_bolsa_testintegracion_completosoloseleccionadas.csv"
-java -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" -jar ${PATH_JAR} --class "coordinador.Principal" "testIntegracion.ExtractorFeatures" "${FEATURES_ELEGIDAS}" "${SG_ENTRADA}" "${TMP_ENTRADA_COLUMNAS_ELEGIDAS}" "10"  1>>/dev/null  2>>${LOG_INTEGRACION}
-head -n 10 "${TMP_ENTRADA_COLUMNAS_ELEGIDAS}" >> ${LOG_INTEGRACION}
-
-echo -e "\nCapa 5 - Intermedio TEMP01:\n" >> ${LOG_INTEGRACION}
-head -n 10 "${SG_ENTRADA}_TEMP01" >> ${LOG_INTEGRACION}
-echo -e "\nCapa 5 - Intermedio TEMP02:\n" >> ${LOG_INTEGRACION}
-head -n 10 "${SG_ENTRADA}_TEMP02">> ${LOG_INTEGRACION}
-echo -e "\nCapa 5 - Intermedio TEMP03:\n" >> ${LOG_INTEGRACION}
-head -n 10 "${SG_ENTRADA}_TEMP03">> ${LOG_INTEGRACION}
-echo -e "\nCapa 5 - Intermedio TEMP04:\n" >> ${LOG_INTEGRACION}
-head -n 10 "${SG_ENTRADA}_TEMP04">> ${LOG_INTEGRACION}
-echo -e "\nCapa 5 - Intermedio TEMP05:\n" >> ${LOG_INTEGRACION}
-head -n 10 "${SG_ENTRADA}_TEMP05">> ${LOG_INTEGRACION}
-echo -e "\nCapa 5 - Intermedio TEMP06:\n" >> ${LOG_INTEGRACION}
-head -n 10 "${SG_REDUCIDO}_TEMP06">> ${LOG_INTEGRACION}
-
-
+echo -e "<br><br>Para analizar el CSV reducido (normalizado, sin nulos y solo con features dinámicas elegidas), veamos las primeras filas del fichero de entrada (COMPLETO.csv) y las primeras filas del fichero reducido (serán esas mismas filas, salvo si tenían nulos, que habrán sido eliminadas y veríamos las siguientes)<br>" >> ${INFORME_OUT}
+echo -e "<br>Las primeras filas de COMPLETO (de la primera empresa que aparece):<br>" >> ${INFORME_OUT}
+head -n 4 "${SG_ENTRADA}" >> ${INFORME_OUT}
 
 SG_REDUCIDO="${DIR_SUBGRUPOS}${SG_ANALIZADO}/REDUCIDO.csv"
-echo -e "\nSubgrupo ${SG_ANALIZADO} - Datos reducidos (normalizar + seleccion de columnas): ${SG_REDUCIDO} --> Tamanio (bytes) = "$(stat -c%s "$SG_REDUCIDO")" con "$(wc -l $SG_REDUCIDO | cut -d\  -f 1)" filas\n" >> ${LOG_INTEGRACION}
+echo -e "<br><br>Las features elegidas han sido:<br>" >> ${INFORME_OUT}
+FEATURES_ELEGIDAS=$(head -n 1 ${SG_REDUCIDO})
+echo -e "${FEATURES_ELEGIDAS}" >> ${INFORME_OUT}
 
-echo -e "\n\nY vemos la transformacion de esas filas en REDUCIDO (fijarse en si la normalización de las columnas tiene sentido!!! ):\n" >> ${LOG_INTEGRACION}
-head -n 10 ${SG_REDUCIDO}>> ${LOG_INTEGRACION}
+echo -e "<br><br>Entonces, nos fijamos solo en esas features elegidas de COMPLETO:<br>" >> ${INFORME_OUT}
+TMP_ENTRADA_COLUMNAS_ELEGIDAS="/tmp/temp_bolsa_testintegracion_completosoloseleccionadas.csv"
+java -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" -jar ${PATH_JAR} --class "coordinador.Principal" "testIntegracion.ExtractorFeatures" "${FEATURES_ELEGIDAS}" "${SG_ENTRADA}" "${TMP_ENTRADA_COLUMNAS_ELEGIDAS}" "10"  1>>/dev/null  2>>${INFORME_OUT}
+head -n 10 "${TMP_ENTRADA_COLUMNAS_ELEGIDAS}" >> ${INFORME_OUT}
 
-echo -e "\nSubgrupo ${SG_ANALIZADO} - Modelo ganador --> "$(ls ${DIR_SUBGRUPOS}${SG_ANALIZADO}/ | grep 'ganador')"\n" >> ${LOG_INTEGRACION}
+echo -e "<br>Capa 5 - Intermedio TEMP01:<br>" >> ${INFORME_OUT}
+head -n 10 "${SG_ENTRADA}_TEMP01" >> ${INFORME_OUT}
+echo -e "<br>Capa 5 - Intermedio TEMP02:<br>" >> ${INFORME_OUT}
+head -n 10 "${SG_ENTRADA}_TEMP02">> ${INFORME_OUT}
+echo -e "<br>Capa 5 - Intermedio TEMP03:<br>" >> ${INFORME_OUT}
+head -n 10 "${SG_ENTRADA}_TEMP03">> ${INFORME_OUT}
+echo -e "<br>Capa 5 - Intermedio TEMP04:<br>" >> ${INFORME_OUT}
+head -n 10 "${SG_ENTRADA}_TEMP04">> ${INFORME_OUT}
+echo -e "<br>Capa 5 - Intermedio TEMP05:<br>" >> ${INFORME_OUT}
+head -n 10 "${SG_ENTRADA}_TEMP05">> ${INFORME_OUT}
+echo -e "<br>Capa 5 - Intermedio TEMP06:<br>" >> ${INFORME_OUT}
+head -n 10 "${SG_REDUCIDO}_TEMP06">> ${INFORME_OUT}
+
+
+
+
+echo -e "<br>Subgrupo ${SG_ANALIZADO} - Datos reducidos (normalizar + seleccion de columnas): ${SG_REDUCIDO} --> Tamanio (bytes) = "$(stat -c%s "$SG_REDUCIDO")" con "$(wc -l $SG_REDUCIDO | cut -d\  -f 1)" filas<br>" >> ${INFORME_OUT}
+
+echo -e "<br><br>Y vemos la transformacion de esas filas en REDUCIDO (fijarse en si la normalización de las columnas tiene sentido!!! ) <br><b>La primera columna es el indice del dataframe. Sirve para poder identificar a que fila del COMPLETO.csv corresponde esta fila con predicciones del REDUCIDO.csv ¿Es correcto?:</b><br><br>" >> ${INFORME_OUT}
+head -n 5 ${SG_REDUCIDO} | sed -z 's/\n/<br><br>\n/g' >> ${INFORME_OUT}
+
+echo -e "<br>Subgrupo ${SG_ANALIZADO} - Modelo ganador --> "$(ls ${DIR_SUBGRUPOS}${SG_ANALIZADO}/ | grep 'ganador')"<br>" >> ${INFORME_OUT}
 
 
 #######################################################################################################
-echo -e "******************************** COMPROBACIONES del FUTURO2 *******************************" >> ${LOG_INTEGRACION}
+echo -e "<h2>******************************** COMPROBACIONES del FUTURO2 *******************************</h2>" >> ${INFORME_OUT}
 
 
 
-
+echo -e "</body></html>"  >> ${INFORME_OUT}
 echo -e "TEST INTEGRACION - FIN: "$( date "+%Y%m%d%H%M%S" )
