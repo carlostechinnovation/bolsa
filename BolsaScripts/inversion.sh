1a0bed4012c478551b4d76e369c934be3a050fab
Carlos <carlosandresgarcia1986@hotmail.com>
2023-08-01 09:19:21 +0200

    HTMLs del futuro (ID ejecucion: 20230731)

--- BolsaScripts/inversion.sh
+++ BolsaScripts/inversion.sh
@@ -61,31 +61,32 @@ DIR_JAVA="${DIR_CODIGOS}BolsaJava/"
 PATH_JAR="${DIR_JAVA}target/bolsajava-1.0-jar-with-dependencies.jar"
 
 
+##############################################################################################################################
 echo -e "$( date "+%Y%m%d%H%M%S" ) [inversion.sh] Se crea carpeta diaria de entregables y se meten dentro..." >>${LOG_INVERSION}
 cd "${DIR_DROPBOX}"
-CARPETA_ENTREGABLES="${DIR_DROPBOX}"$(date '+%Y%m%d_%H%M%S')
-echo "CARPETA_ENTREGABLES=${CARPETA_ENTREGABLES}" >> ${LOG_INVERSION}
-mkdir -p "${CARPETA_ENTREGABLES}"
+DIR_ENTREGABLES_DROPBOX="${DIR_DROPBOX}"$(date '+%Y%m%d_%H%M%S')
+echo "DIR_ENTREGABLES_DROPBOX=${DIR_ENTREGABLES_DROPBOX}" >> ${LOG_INVERSION}
+mkdir -p "${DIR_ENTREGABLES_DROPBOX}"
 PREFIJO_RECIEN_CALCULADOS=$(ls -t | grep GRANDE | grep SG_0_ | head -n 1 | awk -F "_" '{print $1}')  # no tiene que cogerse AAAAMMDD de ahora, sino de los CSV mas recientes encontrados
 echo "PREFIJO_RECIEN_CALCULADOS=${PREFIJO_RECIEN_CALCULADOS}" >> ${LOG_INVERSION}
 
 if [ -z "${PREFIJO_RECIEN_CALCULADOS}" ]; then
     echo "PREFIJO_RECIEN_CALCULADOS esta vacio!!" >>${LOG_INVERSION}
 else
-    echo "Copiando a carpeta entregables..." >>${LOG_INVERSION}
-    cp "${DIR_DROPBOX}/${PREFIJO_RECIEN_CALCULADOS}.html" "${CARPETA_ENTREGABLES}/"
-    cp "${DIR_DROPBOX}/${PREFIJO_RECIEN_CALCULADOS}_todas_las_empresas.html" "${CARPETA_ENTREGABLES}/"
-    cp "/bolsa/logs/pasado_metricas_y_rentabilidades.html" "${CARPETA_ENTREGABLES}/"
-    cp "/bolsa/pasado/empresas_clustering_web.html" "${CARPETA_ENTREGABLES}/"
+    echo "Copiando a carpeta entregables en DROPBOX..." >>${LOG_INVERSION}
+    cp "${DIR_DROPBOX}/${PREFIJO_RECIEN_CALCULADOS}.html" "${DIR_ENTREGABLES_DROPBOX}/"
+    cp "${DIR_DROPBOX}/${PREFIJO_RECIEN_CALCULADOS}_todas_las_empresas.html" "${DIR_ENTREGABLES_DROPBOX}/"
+    cp "/bolsa/logs/pasado_metricas_y_rentabilidades.html" "${DIR_ENTREGABLES_DROPBOX}/"
+    cp "/bolsa/pasado/empresas_clustering_web.html" "${DIR_ENTREGABLES_DROPBOX}/"
 
     ################################## GITHUB: commit and push##############################################################
-    echo -e "$( date "+%Y%m%d%H%M%S" ) [inversion.sh] Haciendo GIT COMMIT..." >>${LOG_INVERSION}
-    #Subir HTML resultado a GIT
+    echo -e "$( date "+%Y%m%d%H%M%S" ) [inversion.sh] Copiando a carpeta entregables en GIT..." >>${LOG_INVERSION}
     DIR_DOCS_HTML_GIT="${DIR_CODIGOS}docs/PREFIJO_RECIEN_CALCULADOS/"
+	echo "Copiando todos los entregables:  ${DIR_ENTREGABLES_DROPBOX} --> ${DIR_DOCS_HTML_GIT}" >>${LOG_INVERSION}
     mkdir -p "${DIR_DOCS_HTML_GIT}"
-    cp "${CARPETA_ENTREGABLES}/*.html" "${DIR_DOCS_HTML_GIT}"
+    cp "${DIR_ENTREGABLES_DROPBOX}/*.html" "${DIR_DOCS_HTML_GIT}"
     
-    cd "${DIR_DOCS_HTML_GIT}"
+	cd "${DIR_DOCS_HTML_GIT}"
     git add "."
     git commit -am "HTMLs del futuro (ID ejecucion: ${PREFIJO_RECIEN_CALCULADOS})"
     git push
